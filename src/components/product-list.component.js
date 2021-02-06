d80cac60356d7e74d92e374d47c9ac216d6dd942
frederick-guo <freddyguo07@gmail.com>
2021-02-06 08:49:23 -0600

    worked on product-list component

--- src/components/product-list.component.js
+++ src/components/product-list.component.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Product = props => (
+    <tr>
+      <td>{props.product.categoryName}</td>
+      <td>{props.product.productName}</td>
+      <td>{props.product.description}</td>
+      <td>{props.product.price}</td>
+      <td>{props.product.paidFor}</td>
+      <td>{props.product.date.substring(0,10)}</td>
+      <td>
+        <Link to={"/edit/"+props.product._id}>edit</Link> | <a href="#" onClick={() => { props.deleteProduct(props.product._id) }}>delete</a>
+      </td>
+    </tr>
+  )
 
 export default class ProductList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteProduct = this.deleteProtect.bind(this);
+
+        this.state = {products: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/products/')
+            .then(response => {
+                this.setState({ products: response.data})
+            })
+            .catach((error) => {
+                console.log(error);
+            })
+    }
+
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/products'+id)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            products: this.state.products.filter(pl => pl._id !== id)
+        })
+    }
+
+    productList() {
+        return this.state.products.map(currentproduct => {
+          return <Product product={currentproduct} deleteProduct={this.deleteProduct} key={currentproduct._id}/>;
+        })
+    }
+
+
     render() {
         return (
-            <div>
-                <p>You are on the ProductList component!</p>
-            </div>
+          <div>
+            <h3>Logged Products</h3>
+            <table className="table">
+              <thead className="thead-light">
+                <tr>
+                  <th>Category</th>
+                  <th>Prodcut</th>
+                  <th>Description</th>
+                  <th>Price</th>
+                  <th>Is it paid for?</th>
+                  <th>Date</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                { this.productList() }
+              </tbody>
+            </table>
+          </div>
         )
-    }
+      }
 }
