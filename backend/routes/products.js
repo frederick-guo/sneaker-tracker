963b6260d7994c5e419b627b4a09bd37d3be0ee6
frederick-guo <freddyguo07@gmail.com>
2021-02-24 22:45:40 -0600

    fixed backend

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let Product = require('../models/product.model');
 
 router.route('/').get((req, res) => {
-  Prodcut.find()
+  Product.find()
     .then(products => res.json(products))
     .catch(err => res.status(400).json('Error: ' + err));
 });
--- src/components/product-list.component.js
+++ src/components/product-list.component.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+
+/*import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -38,7 +39,7 @@ export default class ProductList extends Component {
 
     deleteProduct(id) {
         axios.delete('http://localhost:5000/products'+id)
-            .then(res => console.log(res.data));
+            .then(response => console.log(response.data));
 
         this.setState({
             products: this.state.products.filter(pl => pl._id !== id)
@@ -75,4 +76,80 @@ export default class ProductList extends Component {
           </div>
         )
       }
+}
+*/
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Product = props => (
+    <tr>
+      <td>{props.product.categoryName}</td>
+      <td>{props.product.productName}</td>
+      <td>{props.product.description}</td>
+      <td>{props.product.price}</td>
+      <td>{props.product.paidFor}</td>
+      <td>{props.product.date.substring(0,10)}</td>
+      <td>
+        <Link to={"/edit/"+props.product._id}>edit</Link> | <a href="#" onClick={() => { props.deleteProduct(props.product._id) }}>delete</a>
+      </td>
+    </tr>
+)
+
+export default class ProductList extends Component {
+    constructor (props) {
+        super(props);
+
+        this.deleteProduct = this.deleteProduct.bind(this);
+
+        this.state = {products: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/products/')
+            .then(response => {
+                this.setState({products: response.data})
+        })
+            .catch((error) => {
+                console.log(error);
+        })
+    }
+
+    deleteProduct(id) {
+        axios.delete('http://localhost:5000/products/'+id)
+          .then(response => { console.log(response.data)});
+        
+        this.setState({
+          products: this.state.products.filter(pl => pl._id !== id)
+        })
+    }
+
+    productList() {
+        return this.state.products.map(currentproduct => {
+          return <Product product={currentproduct} deleteProduct={this.deleteProduct} key={currentproduct._id}/>;
+        })
+    }
+
+    render() {
+        return (
+        <div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.productList() }
+          </tbody>
+        </table>
+        </div>
+        )
+    }
 }
